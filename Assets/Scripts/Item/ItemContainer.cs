037d5e6bd9ba4729118fd2e06d6169be78d22016
Scream <scream870102@gmail.com>
2021-07-24 16:54:23 +0800

    ItemSpriteContainer

---
+++ Assets/Scripts/Item/ItemContainer.cs
@@ -0,0 +1,5 @@
+using Scream.UniMO.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemContainer", menuName = "ScriptableObjects/ItemContainer")]
+class ItemContainer : Container<ItemType, Sprite> { }
