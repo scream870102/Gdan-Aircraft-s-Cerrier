fbb7cc1bea2759dd72a493ea2a8642954ed86730
Scream <scream870102@gmail.com>
2021-07-25 13:26:53 +0800

    Unpair fin

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -9,8 +9,8 @@ using Scream.UniMO;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField]Player player1;
-    [SerializeField]Player player2;
+    [SerializeField] Player player1;
+    [SerializeField] Player player2;
     public Text Player1PairText;
     public Text Player2PairText;
 
@@ -20,14 +20,17 @@ public class PlayerManager : MonoBehaviour
     void OnEnable()
     {
         InputUser.onUnpairedDeviceUsed += OnUnpairedDeviceUsed;
+        DomainEvents.Register<OnGameEnd>(HandleGameEnd);
     }
 
     void OnDisable()
     {
         InputUser.onUnpairedDeviceUsed -= OnUnpairedDeviceUsed;
+        DomainEvents.UnRegister<OnGameEnd>(HandleGameEnd);
     }
 
-    void Awake ( ) {
+    void Awake()
+    {
         ++InputUser.listenForUnpairedDeviceActivity;
 
         Player1PairText.enabled = true;
@@ -35,27 +38,30 @@ public class PlayerManager : MonoBehaviour
         Player1PairText.text = "Press Any Key";
         Player2PairText.text = "Press Any Key";
 
-        
+
     }
 
-    void OnUnpairedDeviceUsed (InputControl c, InputEventPtr e) {
-        if (!(c.device.GetType ( ) == Keyboard.current.GetType ( ) || c.device.GetType ( ) == Gamepad.current.GetType ( )))
+    void OnUnpairedDeviceUsed(InputControl c, InputEventPtr e)
+    {
+        if (!(c.device.GetType() == Keyboard.current.GetType() || c.device.GetType() == Gamepad.current.GetType()))
             return;
-        if (!player1.InputUser.valid) {
-            player1.InputUser = InputUser.PerformPairingWithDevice (c.device);
-            player1.InputUser.AssociateActionsWithUser (player1.PlayerInput);
+        if (!player1.InputUser.valid)
+        {
+            player1.InputUser = InputUser.PerformPairingWithDevice(c.device);
+            player1.InputUser.AssociateActionsWithUser(player1.PlayerInput);
             player1Device = c.device;
-            Debug.Log ("Pairing player1 with " + c.device.name);
+            Debug.Log("Pairing player1 with " + c.device.name);
             Player1PairText.enabled = false;
         }
-        else if (!player2.InputUser.valid) {
-            player2.InputUser = InputUser.PerformPairingWithDevice (c.device);
-            player2.InputUser.AssociateActionsWithUser (player2.PlayerInput);
+        else if (!player2.InputUser.valid)
+        {
+            player2.InputUser = InputUser.PerformPairingWithDevice(c.device);
+            player2.InputUser.AssociateActionsWithUser(player2.PlayerInput);
             player2Device = c.device;
-            Debug.Log ("Pairing player2 with " + c.device.name);
+            Debug.Log("Pairing player2 with " + c.device.name);
             Player2PairText.enabled = false;
         }
-        if(player1.InputUser.valid && player2.InputUser.valid)
+        if (player1.InputUser.valid && player2.InputUser.valid)
         {
             DomainEvents.Raise<OnPairComplete>(null);
         }
@@ -65,5 +71,11 @@ public class PlayerManager : MonoBehaviour
     {
         player1.InputUser.UnpairDevice(player1Device);
         player2.InputUser.UnpairDevice(player2Device);
+    }
+
+
+    void HandleGameEnd(OnGameEnd e)
+    {
+        UnPairAllDevices();
     }
 }
